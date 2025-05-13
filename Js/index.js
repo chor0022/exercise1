dabcce8385a8f87b49bcddf76a936d8fa21326d4
chor0022 <chor0022@algonquinlive.com>
2025-05-13 16:11:59 -0400

    Create index.js

---
+++ Js/index.js
@@ -0,0 +1,37 @@
+//Task 1
+let a = 3;
+              let b = 5;
+              let c;
+              
+let output = ` let a = 3;
+              let b = 5;
+              let c;
+              a + b = ${a + b}
+              a - b = ${a - b}
+              a * b = ${a * b}
+              a / b = ${a / b}
+              a % b = ${a % b}
+             (a += b) = ${a += b}
+             (a -= b) = ${a -= b}
+             (a *= b) = ${a *= b}
+             (a /= b) = ${a /= b}
+             (a %= b) = ${a %= b}
+             a == b = ${a == b}
+             a != b = ${a != b}
+             a > b = ${a > b}
+             a < b = ${a < b}
+            !a && !c = ${!a && !c}
+            !a || !c = ${!a || !c}`
+
+console.log(output)
+
+
+// Task 2
+
+let fn = `Vatsal`
+let ln = `Chorasiya`
+let email = `chor0022@algonquinlive.com`
+
+let result = `Hi there! My name is ${fn}${ln} and you can contact me at ${email}`
+
+console.log(result)
